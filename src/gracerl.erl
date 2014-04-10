8ab2b128012513b42e908339963c37f4d289dc68
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2014-04-10 14:05:03 +0200

    Rename: gracerl_app.erl -> gracerl.erl

--- src/gracerl.erl
+++ src/gracerl.erl
@@ -1,13 +1,18 @@
--module(gracerl_app).
+-module(gracerl).
 
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/0, start/2,
+         stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
+
+start() ->
+    application:start(tracerl),
+    application:start(gracerl).
 
 start(_StartType, _StartArgs) ->
     gracerl_sup:start_link().
