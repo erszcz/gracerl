49040652c38c0a49f80f13e020ca2e0b1e614aa9
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2014-04-10 14:28:10 +0200

    Configure target carbon in start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-erl -setcookie ejabberd -sname gracerl -pa ebin deps/*/ebin -s gracerl -gracerl node $@
+erl -setcookie ejabberd -sname gracerl -pa ebin deps/*/ebin -s gracerl \
+    -gracerl carbon_host \"10.100.0.70\" \
+    -gracerl carbon_port 2003 \
+    -gracerl node $@
